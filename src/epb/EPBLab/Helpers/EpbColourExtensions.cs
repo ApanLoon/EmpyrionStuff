b4464f40a389bd004ce17b802591a80672aad9d0
ApanLoon <apanloon@live.co.uk>
2019-03-16 16:02:32 +0100

    Added some common info for every block.

---
+++ src/epb/EPBLab/Helpers/EpbColourExtensions.cs
@@ -0,0 +1,14 @@
+﻿
+using System.Windows.Media;
+using EPBLib.BlockData;
+
+namespace EPBLab.Helpers
+{
+    public static class EpbColourExtensions
+    {
+        public static Color ToColor(this EpbColour c)
+        {
+            return Color.FromRgb(c.R, c.G, c.B);
+        }
+    }
+}
--- src/epb/EPBLab/ViewModel/Tree/BlockNode.cs
+++ src/epb/EPBLab/ViewModel/Tree/BlockNode.cs
@@ -1,6 +1,10 @@
 ﻿
+using System.Linq;
+using System.Windows.Media;
 using System.Windows.Media.Media3D;
+using EPBLab.Helpers;
 using EPBLib;
+using EPBLib.BlockData;
 
 namespace EPBLab.ViewModel.Tree
 {
@@ -9,7 +13,15 @@ namespace EPBLab.ViewModel.Tree
         protected EpbBlock Block;
         protected EpBlueprint Blueprint;
 
+        public string BlockType => Block.BlockType.ToString();
+        public string Variant => Block.VariantName;
+
         public Point3D Position { get; set; }
+
+        public EpbBlock.EpbBlockRotation Rotation => Block.Rotation;
+
+        public Color[] Colours { get; set; }
+        public byte[] Textures => Block.Textures;
 
         public BlockNode(EpbBlock block, EpBlueprint blueprint)
         {
@@ -18,6 +30,7 @@ namespace EPBLab.ViewModel.Tree
             Title = block.BlockType.ToString();
             EpbBlockPos pos = block.Position;
             Position = new Point3D(pos.X - Blueprint.Width / 2.0, pos.Y - Blueprint.Height / 2.0, pos.Z - Blueprint.Depth / 2.0);
+            Colours = Block.Colours.Select(index => Blueprint.Palette[(EpbColourIndex)index].ToColor()).ToArray();
         }
     }
 }
