3002a709943480b183bf74ee93d4efe1b4f7d26a
ApanLoon <apanloon@live.co.uk>
2019-04-27 19:06:24 +0200

    Minor error checking - sometimes threw an exception on exit.

--- src/epb/EPBLab/View/PositionAimPoint2UpDirectionConverter.cs
+++ src/epb/EPBLab/View/PositionAimPoint2UpDirectionConverter.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Globalization;
+using System.Windows;
 using System.Windows.Data;
 using System.Windows.Media.Media3D;
 using EPBLab.Helpers;
@@ -10,6 +11,10 @@ namespace EPBLab.View
     {
         public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
         {
+            if (values[0] == DependencyProperty.UnsetValue || values[1] == DependencyProperty.UnsetValue)
+            {
+                return null;
+            }
             Point3D position = (Point3D)values[0];
             Point3D aimPoint = (Point3D)values[1];
             Vector3D lookDirection = new Vector3D(aimPoint.X - position.X, aimPoint.Y - position.Y, aimPoint.Z - position.Z);
